09c96414c02df541b96a0775e333a26f466e6693
rehnrdnsyh <rehanardiansyah@students.amikom.ac.id>
2025-12-19 14:09:17 +0700

    Mengoreksi bagian header manajamen agar notifikasi dapat tampill

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,10 +2,10 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\View;
 use App\Models\BahanBaku;
 use App\Models\Produk;
+use Illuminate\Support\Facades\View;
+use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -30,6 +30,7 @@ class AppServiceProvider extends ServiceProvider
                 ->get()
                 ->map(function ($item) {
                     $percentage = $item->min_stok > 0 ? round(($item->stok / $item->min_stok) * 100) : 0;
+
                     return [
                         'id' => $item->id,
                         'nama' => $item->nama,
@@ -39,7 +40,7 @@ class AppServiceProvider extends ServiceProvider
                         'percentage' => $percentage,
                         'type' => 'bahan_baku',
                         'status' => $item->stok == 0 ? 'Habis' : ($percentage <= 20 ? 'Kritis' : 'Rendah'),
-                        'color' => $item->stok == 0 ? 'red' : ($percentage <= 20 ? 'red' : 'yellow')
+                        'color' => $item->stok == 0 ? 'red' : ($percentage <= 20 ? 'red' : 'yellow'),
                     ];
                 });
 
@@ -49,6 +50,7 @@ class AppServiceProvider extends ServiceProvider
                 ->get()
                 ->map(function ($item) {
                     $percentage = $item->min_stok > 0 ? round(($item->stok / $item->min_stok) * 100) : 0;
+
                     return [
                         'id' => $item->id,
                         'nama' => $item->nama,
@@ -57,7 +59,7 @@ class AppServiceProvider extends ServiceProvider
                         'percentage' => $percentage,
                         'type' => 'produk',
                         'status' => $item->stok == 0 ? 'Habis' : ($percentage <= 20 ? 'Kritis' : 'Rendah'),
-                        'color' => $item->stok == 0 ? 'red' : ($percentage <= 20 ? 'red' : 'yellow')
+                        'color' => $item->stok == 0 ? 'red' : ($percentage <= 20 ? 'red' : 'yellow'),
                     ];
                 });
 
@@ -70,7 +72,7 @@ class AppServiceProvider extends ServiceProvider
 
             $view->with([
                 'notifications' => $notifications,
-                'notificationCount' => $notificationCount
+                'notificationCount' => $notificationCount,
             ]);
         });
     }
