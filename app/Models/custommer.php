0f8bd0903eb5b889ec08328f99c0e00a6172e88b
rehnrdnsyh <rehanardiansyah@students.amikom.ac.id>
2025-12-24 13:20:56 +0700

    Customer

--- app/Models/custommer.php
+++ app/Models/custommer.php
@@ -7,4 +7,15 @@ use Illuminate\Database\Eloquent\Model;
 class custommer extends Model
 {
     //
+    protected $table = 'data_customer';
+
+    protected $fillable = [
+        'nama',
+        'telepon',
+        'email'
+    ];
+
+    public function DetailTransaksi(){
+        return $this->belongsTo(DetailTransaksi::class, 'id_customer');
+    }
 }
