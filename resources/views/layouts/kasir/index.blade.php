f0172f77582d144422e12b95711c1793593cd4d0
Firmanfebrianto <firmanarwan048@gmail.com>
2026-01-04 04:08:37 +0700

    perbaikan

--- resources/views/layouts/kasir/index.blade.php
+++ resources/views/layouts/kasir/index.blade.php
@@ -9,6 +9,7 @@
     <script src="https://cdn.tailwindcss.com"></script>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
     <script>
         tailwind.config = {
@@ -325,6 +326,9 @@
             restoreSidebarState();
         });
     </script>
+    
+    <!-- Additional Scripts Section -->
+    @yield('scripts')
 </body>
 
 </html>
