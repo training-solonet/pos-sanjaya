e84694c411377a97b2fc3822e727ff7053cbf9c5
AlexanderIII-TheGreat <abiyyu040@gmail.com>
2026-01-12 18:28:41 +0700

    fixed bug and add new pagee

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use App\Http\Controllers\Manajemen\ProdukController;
 use App\Http\Controllers\Manajemen\ProdukGagalController as ManajemenProdukGagalController;
 use App\Http\Controllers\Manajemen\ResepController;
 use App\Http\Controllers\Manajemen\SettingController;
-use App\http\Controllers\Manajemen\ShiftManController;
+use App\Http\Controllers\Manajemen\ShiftManController;
 use App\Http\Controllers\Manajemen\UpdateStokProdukController;
 use App\Http\Controllers\RedirectController;
 use App\Http\Middleware\IsKasir;
@@ -72,8 +72,8 @@ Route::middleware([IsManagement::class])->group(function () {
             'updateproduk' => UpdateStokProdukController::class,
             'opname' => OpnameController::class,
             'setting' => SettingController::class,
-            'shiftman' => ShiftManController::class,
             'produkgagal' => ManajemenProdukGagalController::class,
+            'shiftman' => ShiftManController::class,
         ]);
     });
 });
